b9f4ec5db46501f4fc4d776c3713f4456ba3f951
Jonathan Wylliem <jonathan.wylliem@jurnal.id>
2019-02-16 22:03:49 +0700

    Add travis deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,20 @@
 # build images
-docker build -t jonathanwylliem/multi-client -f ./client/Dockerfile ./client
-docker build -t jonathanwylliem/multi-server -f ./server/Dockerfile ./server
-docker build -t jonathanwylliem/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t jonathanwylliem/multi-client:latest -t jonathanwylliem/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jonathanwylliem/multi-server:latest -t jonathanwylliem/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jonathanwylliem/multi-worker:latest -t jonathanwylliem/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 # push images
-docker push jonathanwylliem/multi-client
-docker push jonathanwylliem/multi-server
-docker push jonathanwylliem/multi-worker
+docker push jonathanwylliem/multi-client:latest
+docker push jonathanwylliem/multi-server:latest
+docker push jonathanwylliem/multi-worker:latest
+
+docker push jonathanwylliem/multi-client:$SHA
+docker push jonathanwylliem/multi-server:$SHA
+docker push jonathanwylliem/multi-worker:$SHA
 
 # apply deployment
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=jonathanwylliem/multi-server
+kubectl set image deployments/server-deployment server=jonathanwylliem/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jonathanwylliem/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jonathanwylliem/multi-worker:$SHA
